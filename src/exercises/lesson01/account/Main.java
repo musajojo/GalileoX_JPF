4b83f097b703fd10fac3adf6d9856a2370a95008
Moses Dee <58441332+musajojo@users.noreply.github.com>
2020-07-09 20:49:40 -0400

    Account

--- src/exercises/lesson01/account/Main.java
+++ src/exercises/lesson01/account/Main.java
@@ -1,9 +1,14 @@
 package exercises.lesson01.account;
-
 /**
  * Main class of the Java program.
  */
 
+/*
+Instructions:
+
+Add a method on account named "withdraw" that removes an 'amount' of money given on the parameters,
+only if the balance has  enough money and return true, if not return false.
+*/
 public class Main {
 
     public static void main(String[] args) {
