76df348b8923edf4724a57079f30b054da0f2027
Moses Dee <58441332+musajojo@users.noreply.github.com>
2020-07-18 17:04:41 -0400

    Coding Excercise - 03 - Fix-It

---
+++ src/exercises/gradedExercises/codingExcercise03_Fix_It/Main.java
@@ -0,0 +1,39 @@
+package exercises.gradedExercises.codingExcercise03_Fix_It;
+public class Main {}
+/*
+Instructions:
+
+The following program has errors, compile and run the program to check the errors.
+Be careful before running the program, it may have infinite loops.
+After finding the errors,
+respond the questions below and select the right answers to find out if you discovered  the errors.
+ */
+
+
+
+///**
+//
+// * Exercise 3 - Fix It (Hands-on Work)
+// */
+//
+//import exercises.gradedExercises.codingExcercise03_Fix_It.Passport;
+//
+//import java.util.Date;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        //Date(int year, int month, int date)
+//        Date birthDate = new Date(1987,0,22); //Remember to subtract 1 to the month
+//        Date expirationDate = new Date(2017,0,13)
+//        long id = 1287564540101L;
+//        Passport myPassport = new Passport(id, "Conny", "Smith", Austrian, birthDate, expirationDate, "F");
+//        boolean passportExpired = myPassport.isExpired();
+//
+//        if (passportExpired == true) {
+//            System.out.println("You need to renew your passport")
+//        } else {
+//            System.out.println("Your passport is still valid")
+//        }
+//    }
+//}
---
+++ src/exercises/gradedExercises/codingExcercise03_Fix_It/Passport.java
@@ -0,0 +1,64 @@
+package exercises.gradedExercises.codingExcercise03_Fix_It;
+public class Passport {}
+/*
+Instructions:
+
+The following program has errors, compile and run the program to check the errors.
+Be careful before running the program, it may have infinite loops.
+After finding the errors,
+respond the questions below and select the right answers to find out if you discovered  the errors.
+ */
+
+
+//import java.util.Date;
+//
+//public class Passport {
+//    int passportNumber;
+//    String lastName, firstNames, nationality;
+//    Date dateBirth, expirationDate;
+//    char gender; //'F' for female, 'M' for male
+//
+//    public Passport (long id, String first, String last, String nationality, Date birthdate, Date expiration, char gender) {
+//        this.passportNumber = id;
+//        this.lastName = last;
+//        this.firstNames = first;
+//        this.nationality = nationality;
+//        dateBirth = birthdate;
+//        expirationDate = expiration;
+//        this.gender = gender;
+//    }
+//
+//    public char getGender() {
+//        return this.gender;
+//    }
+//
+//    public String getFullName() {
+//        return this.firstNames + " " + this.lastName;
+//    }
+//
+//    public boolean isExpired() {
+//        Date today = new Date();
+//
+//        //the current year represented by this date, minus 1900 for today.
+//        int currentYear = today.getYear() + 1900;
+//        int expirationYear = expirationDate.getYear();
+//
+//        if (expirationYear < currentYear) {
+//            return true;
+//        }
+//
+//        while (currentYear == expirationYear) {
+//            int currentMonth = today.getMonth();
+//            int expirationMonth = expirationDate.getMonth();
+//            if (currentMonth > expirationMonth) {
+//                return true;
+//            } else if (currentMonth == expirationMonth) {
+//                int currentDay = today.getDate();
+//                int expirationDay = expirationDate.getDate();
+//                if (currentDay >= expirationDay) {
+//                    return true;
+//                }
+//            }
+//        }
+//    }
+//}
